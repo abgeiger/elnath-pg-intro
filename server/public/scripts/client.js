89b790a48ec707d9c7bbac28228ad8ee111825a5
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-21 17:00:16 -0600

    not finished

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,37 @@ console.log('js')
 
 $(document).ready(function() {
     console.log('JQ');
+    $('#sumbmitButton').on('click', addNewShoes);
+    $('#shoeList').on('click', '.deleteButton', removeShoe);
+    getShoes();
+});
+
+function getShoes() {
+    $.ajax({
+        method: 'GET',
+        url: '/shoes',
+        success: function(response) {
+            console.log('shoes are', response);
+            $('#shoelist').empty();
+            for (var index = 0; index < response.length; index++) {
+                var shoe = response[index];
+                var $newShoeItem = $('<li>' + shoe.name + ': ' + shoe.cost + '</li>');
+                var $deleteShoeButton = $('<button class="deleteButton">Delete</button>');
+                $deleteShoeButton.data('id', shoe.id);
+                $newShoeItem.append($deleteShoeButton);
+                $('#shoelist').append($newShoeItem);
+            }
+        }
+    });
+}
+
+function removeShoe() {
+    var shoeIdToRemove = $(this).data().id;
+    console.log('remove shoe was clicked', shoeIdToRemove);
+    
+}
+
+function addNewShoes() {
     $.ajax({
         method: 'POST',
         url: '/shoes',
@@ -11,7 +42,7 @@ $(document).ready(function() {
         },
         success: function(response) {
             console.log('response', response);
-
+            getShoes();
         }
-    })
-})
+    });
+}
