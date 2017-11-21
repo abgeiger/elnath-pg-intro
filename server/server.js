8f2a4c669ea90123fb378d01cff211fd03d94696
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-21 13:04:44 -0600

    full stack GET request

--- server/server.js
+++ server/server.js
@@ -8,11 +8,40 @@ var port = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));  // middleware
 app.use(express.static('server/public'));
 
-var shoes = [{name: 'nike', cost: '75'}]
+var config = {
+    database: 'shoe_store', // name of database
+    host: 'localhost', // where is your database (which computer)
+    port: 5432, // port number for database
+    max: 10, // how many connections at one time
+    idleTimeoutMillies: 30000 // 30 seconds to try to connect to database
+};
+
+var pool = new pg.Pool(config);
 
 // for localhost:5000/shoes should return an array of shoe objects
 app.get('/shoes', function(req, res) {
-    res.send(shoes);
+    // attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done) {
+        if(errorConnectingToDatabase) {
+            // there was an error connecting to database
+            console.log('Error connecting to database', errorConnectingToDatabase)
+            res.sendStatus(500);
+        } else {
+            // we connected to the database!!!
+            // now we're going to GET things from the DB
+            client.query('SELECT * FROM shoes;', function(errorMakingQuery, result) {
+                done();
+                if(errorMakingQuery) {
+                    // query failed. Did you test it in Postico?
+                    // log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.send(result.rows);
+                }
+            });
+        }
+    });
 });
 
 app.listen(port, function(){
