223e9ccdda5757f68dfba0c9d58aa58f3e932599
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-21 10:25:44 -0600

    sending hardcoded array on shoes get request

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ var port = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));  // middleware
 app.use(express.static('server/public'));
 
-// Start up our server
+var shoes = [{name: 'nike', cost: '75'}]
+
+// for localhost:5000/shoes should return an array of shoe objects
+app.get('/shoes', function(req, res) {
+    res.send(shoes);
+});
+
 app.listen(port, function(){
   console.log('server is listening on port:', port);
 });
