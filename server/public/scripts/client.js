41dc9b4b8f9eb00e56ee592d126198839bc46962
abgeiger <30698092+abgeiger@users.noreply.github.com>
2017-11-21 13:30:45 -0600

    create POST request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,17 @@
 console.log('js')
 
 $(document).ready(function() {
-    console.log('JQ')
+    console.log('JQ');
+    $.ajax({
+        method: 'POST',
+        url: '/shoes',
+        data: {
+            name: 'Nike Air Jordan',
+            cost: '110'
+        },
+        success: function(response) {
+            console.log('response', response);
+
+        }
+    })
 })
--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ var pool = new pg.Pool(config);
 app.get('/shoes', function(req, res) {
     // attempt to connect to database
     pool.connect(function(errorConnectingToDatabase, client, done) {
-        if(errorConnectingToDatabase) {
+        if (errorConnectingToDatabase) {
             // there was an error connecting to database
             console.log('Error connecting to database', errorConnectingToDatabase)
             res.sendStatus(500);
@@ -31,13 +31,39 @@ app.get('/shoes', function(req, res) {
             // now we're going to GET things from the DB
             client.query('SELECT * FROM shoes;', function(errorMakingQuery, result) {
                 done();
-                if(errorMakingQuery) {
+                if (errorMakingQuery) {
                     // query failed. Did you test it in Postico?
                     // log the error
                     console.log('Error making query', errorMakingQuery);
                     res.sendStatus(500);
                 } else {
                     res.send(result.rows);
+                }
+            });
+        }
+    });
+});
+
+app.post('/shoes', function(req, res) {
+    // attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done) {
+        if (errorConnectingToDatabase) {
+            // there was an error connecting to database
+            console.log('Error connecting to database', errorConnectingToDatabase)
+            res.sendStatus(500);
+        } else {
+            // we connected to the database!!!
+            // now we're going to GET things from the DB
+            client.query(`INSERT INTO shoes (name, cost)
+                VALUES ($1, $2);`, [req.body.name, req.body.cost], function(errorMakingQuery, result) {
+                done();
+                if (errorMakingQuery) {
+                    // query failed. Did you test it in Postico?
+                    // log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(201);
                 }
             });
         }
